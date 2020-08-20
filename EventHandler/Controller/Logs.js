82ac636341a6bb56e6576e6f35cfd1cfdf360c8b
MrGamingBR <67605518+MrGamingBR@users.noreply.github.com>
2020-08-19 21:47:08 -0300

    Update Logs.js

--- EventHandler/Controller/Logs.js
+++ EventHandler/Controller/Logs.js
@@ -1,1 +1,8 @@
-
+module.exports.run = async(bot, message, args, dc, err) => {
+    const embed = new dc.MessageEmbed()
+    .setTitle(`Event Log`)
+    .setDescription(`Deu algum erro ao executar o Comando, Desculpe pela a inconveniencia :pensive:`)
+    .addField(`Erro: `, `${err}`)
+    .setFooter(`Solicitado por ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, size: 2048 }))
+   return message.channel.send(embed)
+}
