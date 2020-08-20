85908de93d930bf668fa03da925f2d0717f25995
MrGamingBR <67605518+MrGamingBR@users.noreply.github.com>
2020-08-19 22:15:40 -0300

    Update handler.js

--- EventHandler/Controller/handler.js
+++ EventHandler/Controller/handler.js
@@ -12,7 +12,7 @@ const ac = command
    return Fun.run(bot, message, args, command, queue)
 }
 // Miscelanea
-if(ac === configM.a || ac === configM.aa || ac === configM.b || ac === configM.bb || ac === configM.c || ac === configM.cc || ac === configM.d || ac === configM.dd || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. || ac === configM. ) {
+if(ac === configM.a || ac === configM.aa || ac === configM.b || ac === configM.bb || ac === configM.c || ac === configM.cc || ac === configM.d || ac === configM.dd || ac === configM.ddd || ac === configM.dddd || ac === configM.e || ac === configM.ee || ac === configM.f || ac === configM.ff || ac === configM.g || ac === configM.gg || ac === configM.ggg || ac === configM.h || ac === configM.hh || ac === configM.i || ac === configM.ii || ac === configM.k || ac === configM.kk || ac === configM.kkk || ac === configM.kkkk || ac === configM.l || ac === configM.ll || ac === configM.m || ac === configM.mm) {
   const Misc = require('../Miscelanea')
   delete require.cache[require.resolve(`../Miscelanea`)];
   return Misc.run(bot, message, args, command, queue)
@@ -280,7 +280,7 @@ if(command === 'start') {
 
 } catch (err) {
   console.error("Erro:  " + err)
-  const logs = require('./logs')
+  const logs = require('./Logs')
   const dc = require('discord.js')
   return logs.run(bot, message, args, dc, err)
   }
