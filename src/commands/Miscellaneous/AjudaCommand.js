48a3e7144a137e98dda7f5c92328a513f200a45a
MrGamingBR <arthurphelipe112@gmail.com>
2020-09-26 18:24:18 -0300

    Teste de novas corre├º├Áes

--- src/commands/Miscellaneous/AjudaCommand.js
+++ src/commands/Miscellaneous/AjudaCommand.js
@@ -16,7 +16,7 @@ module.exports = class HelpCommand extends Command {
 
     const commands = this.client.commands.filter(({ hide, Developer }) => !hide && !Developer)
     const commandPerCategory = (category) => commands.filter(cmd => cmd.category === category)
-    const mapCommand = (command) => `\`${command.name}\`, \`${command.aliases}\`, \`${command.usage}\`, \`${command.category}\``
+    const mapCommand = (command) => `\`${command.name}\``
 
 
     const EmbedAjuda = new MessageEmbed()
@@ -27,11 +27,7 @@ module.exports = class HelpCommand extends Command {
       👥 | Administração [${commandPerCategory('Admin').size}]
       ${commandPerCategory('Admin').map(mapCommand).join(', ')}
 
-      `)
-		 .setFooter(`Solicitado por ${author.username}`, author.displayAvatarURL({ dynamic: true, size: 2048 }))
-	   channel.send(EmbedAjuda)
-  }
-  /*  😂 | Diversão [${commandPerCategory('Fun').size}]
+      😂 | Diversão [${commandPerCategory('Fun').size}]
       ${commandPerCategory('Fun').map(mapCommand).join(', ')}
 
       👮‍♂️| Moderação [${commandPerCategory('Moderation').size}]
@@ -46,5 +42,10 @@ module.exports = class HelpCommand extends Command {
       🛠️ | Utilitários [${commandPerCategory('Miscellaneous').size}]
       ${commandPerCategory('Miscellaneous').map(mapCommand).join(', ')}
 
+      `)
+		 .setFooter(`Solicitado por ${author.username}`, author.displayAvatarURL({ dynamic: true, size: 2048 }))
+	   channel.send(EmbedAjuda)
+  }
+  /*
    */
 }
