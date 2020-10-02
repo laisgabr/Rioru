c3d8207b42e0b9f30be21523dcb79817f88f4248
MrGamingBR <arthurphelipe112@gmail.com>
2020-10-01 21:25:37 -0300

    Porra,  Dnv mano sou burro d+

--- src/listeners/Client/GuildCreateListener.js
+++ src/listeners/Client/GuildCreateListener.js
@@ -39,7 +39,7 @@ module.exports = class GuildCreateListener extends Listener {
        Número de Membros:
        ${guild.memberCount}
 
-       Agora temos ${this.client.guilds.cache.size} Servidores!
+       Agora temos ${this.guilds.cache.size} Servidores!
        `)
        .setColor('GREEN')
        .setThumbnail(guild.iconURL({ dynamic: true }))
