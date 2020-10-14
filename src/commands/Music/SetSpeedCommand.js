b8c9b5ad899630d7e7618f046ffbee59f13ff6cd
MrGamingBR <arthurphelipe112@gmail.com>
2020-10-14 10:06:46 -0300

    Fixed Two errs

--- src/commands/Music/SetSpeedCommand.js
+++ src/commands/Music/SetSpeedCommand.js
@@ -2,7 +2,7 @@ const { Command } = require('../../structure')
 const { ZoePlayer } = require('../../Music')
 const customPlayer = new ZoePlayer()
 
-module.exports = class VaporwaveCommand extends Command {
+module.exports = class SetSpeedCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'setspeed',
@@ -14,9 +14,9 @@ module.exports = class VaporwaveCommand extends Command {
         })
     }
     run ({ channel, args }) {
-        const off = args[0]
-      if(!off) return  channel.send("<:xSweet:756989900661850182> | Diga [on/off] para continuar")
-
+     //   const off = args[0]
+     // if(!off) return  channel.send("<:xSweet:756989900661850182> | Diga [on/off] para continuar")
+        channel.send('Em construção....')
     //  if(off === 'on') {} else if(off === 'off') {} else { c }
     }
 }
