a6d56485881e3252614d8ee453a98762b9c5556d
MrGamingBR <arthurphelipe112@gmail.com>
2020-10-15 14:30:17 -0300

    Fixed Filter Nightcore

--- src/commands/Music/NightcoreCommand.js
+++ src/commands/Music/NightcoreCommand.js
@@ -1,5 +1,4 @@
 const { Command } = require('../../structure')
-const { ZoePlayer } = require('../../Music')
 
 module.exports = class NightcoreCommand extends Command {
   constructor(client) {
@@ -15,17 +14,15 @@ module.exports = class NightcoreCommand extends Command {
   run ({ channel, member, guild, lavalink, args }) {
     const player = lavalink.players.get(guild.id);
 
-    const customPlayer = new ZoePlayer()
-
     const off = args[0]
     if(!off) return  channel.send("<:xSweet:756989900661850182> | Diga [on/off] para continuar")
 
     if(off === 'on') {
-      customPlayer.setNightcore(true)
+      player.setNightcore(true)
      // player.setEQ(Array(6).fill(0).map((n, i) => ({ band: i, gain: 0.55 })));
       return channel.send(`Filtro Nightcore foi ativado!`);
     } else if(off === 'off') {
-      customPlayer.setNightcore(false)
+      player.setNightcore(false)
      // player.setEQ(Array(6).fill(0).map((n, i) => ({ band: i, gain: 0.0 })));
       return channel.send(`Filtro Nightcore foi desativado!`);
     } else {
