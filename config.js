2050e71d1825de0dca06a62b6725dd059f78c624
Miguel Moraes <51008063+zMigueel@users.noreply.github.com>
2020-10-18 02:49:57 +0000

    Removing Boilerplate

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  fetchAllMembers: true,
-
-  token: process.env.TOKEN,
-  owners: JSON.parse(process.env.OWNERS),
-  nodes: JSON.parse(process.env.LAVALINK_NODES),
-  dbConfig: JSON.parse(process.env.FIREBASE_CONFIG)
-}
