6e6232344046b9a645452b42e347b01e2d8da7ef
MrGamingBR <arthurphelipe112@gmail.com>
2020-10-29 21:44:22 -0300

    Add Intents

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const { Intents } = require('discord.js')
+
 const ZoeFileStart = require('./src/Zoe')
 const config = require('./config')
 
-const Zoe = new ZoeFileStart(config, { disableEveryone: true, fetchAllMembers: true })
+const Zoe = new ZoeFileStart(config, { disableEveryone: true, fetchAllMembers: true, ws: { intents: Intents.ALL }})
 
 Zoe.loadCommands('./src/commands')
 
