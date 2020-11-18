04b5768098d6135442a8c379f459aab805771c05
MrGamingBR <arthurphelipe112@gmail.com>
2020-11-18 12:34:02 -0300

    Simple Updates in Zoe

--- src/Commands/Utils/PingCommand.js
+++ src/Commands/Utils/PingCommand.js
@@ -4,11 +4,15 @@ module.exports = class PingCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'ping',
-            aliases: ['pong']
+            aliases: ['pong'],
+            description: 'Mostra o meu Ping',
+            usage: '<prefix>ping',
+            category: 'Miscellaneous',
+            cooldown: 5
         })
     }
 
     run(client, msg, args, t) {
-        client.createMessage(msg.channel.id, `Ping ? \`Hoje não tem ping\``)
+        client.createMessage(msg.channel.id, t('commands:PingCmd.Sucess', { ping: client.shards.get(0).latency }))
     }
 }
