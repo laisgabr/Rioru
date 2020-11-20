7166ed437cdce2ece776d6bf221b5668d88b92c4
MrGamingBR <arthurphelipe112@gmail.com>
2020-11-20 20:05:04 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,15 +9,31 @@ module.exports = {
     
     getAllUsers: true, 
     
-    intents: ["guilds", "guildMessages"], 
-
+    intents: [
+        "guilds",
+        "guildMembers", 
+        "guildBans", 
+        "guildEmojis", 
+        "guildIntegrations", 
+        "guildWebhooks",  
+        "guildInvites" ,
+        "guildVoiceStates", 
+        "guildPresences", 
+        "guildMessages", 
+        "guildMessageReactions",
+        "guildMessageTyping"  ,
+        "directMessages" ,
+        "directMessageReactions",
+        "directMessageTyping"
+    ], 
+    
     token: process.env.BOT_TOKEN,
-
+    
     owners: JSON.parse(process.env.OWNERS),
-
+    
     nodes: JSON.parse(process.env.LAVALINK_NODES),
-
+    
     mongo: process.env.MONGO_URI,
-
+    
     fortnitekey: process.env.FORTNITE_KEY
 }
