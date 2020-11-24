7f9f18acd4cf8fb850c0d87615c4c0d92f436663
MrGamingBR <arthurphelipe112@gmail.com>
2020-11-24 15:30:24 -0300

    Update Files | Goodbye Pocolho

---
+++ src/Structures/System/AntiSpam.js
@@ -0,0 +1,26 @@
+module.exports = class AntiSpam {
+    constructor(client, message) {
+        this.client = client;
+    
+        const AntiSpam = require('discord-anti-spam');
+        
+        const antiSpam = new AntiSpam({
+            warnThreshold: 3, 
+            kickThreshold: 7,
+            banThreshold: 7,
+            maxInterval: 2000, 
+            warnMessage: '{@user}, Please stop spamming.', 
+            kickMessage: '**{user_tag}** has been kicked for spamming.', 
+            banMessage: '**{user_tag}** has been banned for spamming.',
+            maxDuplicatesWarning: 7,
+            maxDuplicatesKick: 10,
+            maxDuplicatesBan: 12, 
+            exemptPermissions: ['ADMINISTRATOR'], 
+            ignoreBots: false,
+            verbose: true,
+            ignoredUsers: []
+        });
+        
+        module.exports = antiSpam;
+    }
+}
