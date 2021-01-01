8e0cb13cd40556d00f2bc6ae02acd66c38e1e855
MrGamingBR <arthurphelipe112@gmail.com>
2021-01-01 14:24:01 -0300

    Simple Commit

--- app/src/main/java/me/zoe/bot/listeners/client/OnMessageReceivedListener.java
+++ app/src/main/java/me/zoe/bot/listeners/client/OnMessageReceivedListener.java
@@ -3,15 +3,19 @@ package me.zoe.bot.listeners.client;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Arrays;
+
 public class OnMessageReceivedListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        String prefix = "";
+        String prefix = "zoe!";
         if(event.getChannelType().toString().toUpperCase() != "TEXT") return;
         if(event.getAuthor().isBot()) return;
 
         if(!event.getMessage().getContentRaw().startsWith(prefix)) return;
         String[] args = event.getMessage().getContentRaw().replaceFirst(prefix, "").trim().split(" ");
-       // Criar uma variavel de tipo string com função de remover a primeiro elemento do array args
+        event.getChannel().sendMessage("" + Arrays.toString(args).replace("[", "").replace("]", "").replace(",", "")).queue();
+
+        String command = args[0];
     }
 }
--- app/src/main/java/me/zoe/bot/loaders/CommandLoader.java
+++ app/src/main/java/me/zoe/bot/loaders/CommandLoader.java
@@ -2,5 +2,6 @@ package me.zoe.bot.loaders;
 
 public class CommandLoader {
     public CommandLoader() {
+
     }
 }
